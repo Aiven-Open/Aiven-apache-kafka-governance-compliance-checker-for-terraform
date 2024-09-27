fa6a16806e329461712f149ccc3028ed58b959ed
Timo Riski <timo.riski@aiven.io>
2024-09-27 17:16:34 +0300

    feat: refactor main_test.go

--- main_test.go
+++ main_test.go
@@ -1,90 +1,80 @@
 package main
 
 import (
-	"testing"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"encoding/json"
+	"testing"
 )
 
-type (
-	TestCase struct {
-		Name	string
-		Args	Args
-		ExpectStdout	string
-		ExpectStderr	string
-	}
-	Args struct {
-		Requester	string
-		Approvers	string
-		Plan		string
-	}
-)
+type TestCase struct {
+	Name         string
+	Args         Args
+	ExpectStdout string
+	ExpectStderr string
+}
+
+type Args struct {
+	Requester string
+	Approvers string
+	Plan      string
+}
 
 func TestMain(t *testing.T) {
-	dir, _ := os.Getwd()
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get working directory: %v", err)
+	}
 
-	tests := make([]TestCase, 0)
-	tests = append(tests, getBasicTest())
+	tests := []TestCase{
+		{
+			Name: "Basic",
+			Args: Args{
+				Requester: "alice",
+				Approvers: "bob,charlie",
+				Plan:      "data/plan.json",
+			},
+			ExpectStdout: "{\"ok\":true,\"errors\":[]}\n",
+			ExpectStderr: "",
+		},
+	}
 
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
-			args := make([]string, 0)
-			args = append(args, "run")
-			args = append(args, "main.go")
-			args = append(args, fmt.Sprintf("-requester=%s", test.Args.Requester))
-    		args = append(args, fmt.Sprintf("-approvers=%s", test.Args.Approvers))
-    		args = append(args, fmt.Sprintf("-plan=%s", filepath.Join(dir, test.Args.Plan)))
-
-			var stdoutBuffer strings.Builder
-			var stderrBuffer strings.Builder
-
-			cmd := exec.Command("go", args...)
-			cmd.Stdout = &stdoutBuffer
-			cmd.Stderr = &stderrBuffer
-			err := cmd.Run()
-			if err != nil {
-				t.Errorf("%s", err.Error())
+			stdout, stderr, runErr := runCommand(dir, test.Args)
+			if runErr != nil {
+				t.Fatalf("Command execution failed: %v", runErr)
 			}
 
-			if test.ExpectStdout != "" {
-				stdout := stdoutBuffer.String()
-				if test.ExpectStdout != stdout {
-					t.Errorf("%s != %s", test.ExpectStdout, stdout)
-				}
-			}
-
-			if test.ExpectStderr != "" {
-				stderr := stderrBuffer.String()
-				if test.ExpectStderr != stderr {
-					t.Errorf("%s != %s", test.ExpectStderr, stderr)
-				}
-			}
-
+			assertOutput(t, "stdout", stdout, test.ExpectStdout)
+			assertOutput(t, "stderr", stderr, test.ExpectStderr)
 		})
 	}
 }
 
-func getBasicTest() TestCase {
-	errors := make([]ResultError, 0)
-	errors = append(errors, ResultError{
-		Error: "approval is required from a member of the owner group",
-		Address: "aiven_kafka_topic.foo",
-	})
-	stdout, _ := json.Marshal(Result{
-		Ok: false,
-		Errors: errors,
-	})
-	return TestCase{ 
-		Name: "Basic", 
-		Args: Args{
-			Requester: "alice", 
-			Approvers: "bob,charlie",
-			Plan: "testdata/plan.json",
-		},
-		ExpectStdout: string(stdout[:]),
+func runCommand(dir string, args Args) (string, string, error) {
+	cmdArgs := []string{
+		"run",
+		"main.go",
+		fmt.Sprintf("-requester=%s", args.Requester),
+		fmt.Sprintf("-approvers=%s", args.Approvers),
+		fmt.Sprintf("-plan=%s", filepath.Join(dir, args.Plan)),
+	}
+
+	var stdoutBuffer, stderrBuffer strings.Builder
+
+	cmd := exec.Command("go", cmdArgs...)
+	cmd.Stdout = &stdoutBuffer
+	cmd.Stderr = &stderrBuffer
+
+	runErr := cmd.Run()
+	return stdoutBuffer.String(), stderrBuffer.String(), runErr
+}
+
+func assertOutput(t *testing.T, name, actual, expected string) {
+	if expected != "" && actual != expected {
+		t.Errorf("Expected %s: %s, got: %s", name, expected, actual)
 	}
 }
